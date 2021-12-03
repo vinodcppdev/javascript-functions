bdd43042b1d0959330f46ab0f3c46ecd4e297911
Vinod Sahu <vinodcppdev@outlook.com>
2021-12-03 11:26:31 +0530

    complete

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -66,8 +66,8 @@ const willBeAlive = (cell, state) => {
   )
 };
 
-const calculateNext = (state) => { 
-  onst { bottomLeft, topRight } = corners(state);
+const calculateNext = (state) => {
+  const { bottomLeft, topRight } = corners(state);
   let result = [];
   for (let y = topRight[1] + 1; y >= bottomLeft[1] - 1; y--) {
     for (let x = bottomLeft[0] - 1; x <= topRight[0] + 1; x++) {
